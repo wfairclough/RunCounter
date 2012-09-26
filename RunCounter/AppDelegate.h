f97d7846cb95378c39deb948383565238bdcef2c
Will Fairclough <wfairclough@gmail.com>
2012-09-25 20:42:26 -0400

    Initial Commit

---
+++ RunCounter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RunCounter
+//
+//  Created by Will Fairclough on 2012-09-25.
+//  Copyright (c) 2012 Will Fairclough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RunCounter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RunCounter
+//
+//  Created by Will Fairclough on 2012-09-25.
+//  Copyright (c) 2012 Will Fairclough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
