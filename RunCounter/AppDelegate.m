04e158f993b1550a597663deb2609d0745411adf
Will Fairclough <wfairclough@gmail.com>
2013-01-13 22:19:02 -0500

    Added Splash Screen and fade animation

--- RunCounter/AppDelegate.m
+++ RunCounter/AppDelegate.m
@@ -11,16 +11,57 @@
 #import "ViewController.h"
 
 @implementation AppDelegate
+@synthesize splashImageView;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    CGRect screenBounds = [[UIScreen mainScreen] bounds];
+
+    
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    
+    self.splashImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, -20, screenBounds.size.width, screenBounds.size.height)]; // Bounds for iPhone 5
+
+    if (screenBounds.size.height == 568) {
+        // iPhone 5 - 4 inch screen
+        [self.splashImageView setImage:[UIImage imageNamed:@"Default-568h@2x"]];
+    } else {
+        // iPhone 4 - 3.5 inch screen
+        [self.splashImageView setImage:[UIImage imageNamed:@"Default@2x"]];
+    }
+    [[self.viewController view] addSubview:self.splashImageView];
+    [[self.viewController view] bringSubviewToFront:self.splashImageView];
+    
+
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     [NSNotificationCenter defaultCenter];
+    
+    // Schedule the fading of the Splash Screen
+    [NSTimer scheduledTimerWithTimeInterval:2.0
+                                     target:self
+                                   selector:@selector(fadeSplashScreen)
+                                   userInfo:nil
+                                    repeats:NO];
+
+    
     return YES;
+}
+
+// Fade the splash screen and remove from Superview
+- (void) fadeSplashScreen
+{
+    //now fade out splash image
+    [UIView transitionWithView:self.window duration:1.0f options:UIViewAnimationOptionTransitionNone
+                                                      animations:^(void){
+                                                          self.splashImageView.alpha=0.0f;
+                                                      }
+                                                      completion:^(BOOL finished){
+                                                          [self.splashImageView removeFromSuperview];
+                                                      }];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
